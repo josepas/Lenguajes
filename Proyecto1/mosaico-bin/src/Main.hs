d72780aa7a3e04ee6395b860122fc3b267925f26
Jose Ricardo Pascarella Quijada <josepas@gringa.gringa.com.ve>
2015-05-12 23:17:08 -0430

    no compila, esqueleto de ciclo en main

--- Proyecto1/mosaico-bin/src/Main.hs
+++ Proyecto1/mosaico-bin/src/Main.hs
@@ -6,8 +6,18 @@ import Graphics.Mosaico.Ventana  (Ventana, cerrar, crearVentana, leerTecla, most
 
 import Diagramas (Orientación(Horizontal, Vertical), caminar, dividir, rectánguloImagen, sustituir)
 
+--define GDK_KEY_KP_Left 0xff96
+--define GDK_KEY_KP_Up 0xff97
+--define GDK_KEY_KP_Right 0xff98
+--define GDK_KEY_KP_Down 0xff99
+--define GDK_KEY_q 0x071
+-- BackSpace
+
 ciclo :: Ventana -> Diagrama -> [Paso] -> IO ()
-ciclo = undefined
+ciclo vent = 
+
+
 
 main :: IO ()
 main = undefined
+
