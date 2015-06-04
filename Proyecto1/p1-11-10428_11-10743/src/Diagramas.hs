ce8e9765e94e3becf34a867644053638e3d07add
Jose Ricardo Pascarella Quijada <josepas@gringa.gringa.com.ve>
2015-06-04 10:30:11 -0430

    ?

---
+++ Proyecto1/p1-11-10428_11-10743/src/Diagramas.hs
@@ -0,0 +1,63 @@
+module
+  Diagramas
+  ( rectánguloImagen
+  , Orientación(Horizontal, Vertical)
+  , dividir
+  , caminar
+  , sustituir
+  )
+  where
+
+import Graphics.Mosaico.Diagrama (Diagrama((:-:), (:|:), Hoja), Paso(Primero, Segundo), Rectángulo(Rectángulo, color, imagen))
+import Graphics.Mosaico.Imagen   (Imagen(Imagen, altura, anchura, datos))
+
+import Imagen (colorPromedio, hSplit, vSplit)
+
+
+
+rectánguloImagen :: Imagen -> Rectángulo
+rectánguloImagen imag = Rectángulo {color = colorPromedio imag, imagen= imag}
+
+data Orientación
+  = Horizontal
+  | Vertical
+  deriving Show
+
+dividir :: Orientación -> Rectángulo -> Maybe Diagrama
+dividir 
+  orientacion 
+  rectángulo 
+  = case esValida orientacion (anchura ima) (altura ima) of
+                  False -> Nothing 
+                  True  -> case orientacion of 
+                    Horizontal -> Just (Hoja (rectánguloImagen arriba) :-: Hoja (rectánguloImagen abajo))
+                    Vertical -> Just (Hoja (rectánguloImagen izquierda) :|: Hoja (rectánguloImagen derecha))
+          where
+            esValida Horizontal _ 1 = False
+            esValida Vertical 1 _   = False
+            esValida _ _ _          = True
+            (arriba, abajo) = hSplit ima
+            (izquierda, derecha) = vSplit ima
+            ima = (imagen rectángulo)
+
+caminar :: [Paso] -> Diagrama -> Maybe Diagrama
+caminar []     diagrama          = Just diagrama
+caminar _ (Hoja _)               = Nothing
+caminar (x:xs) (primero :-: segundo)  = case x of
+                                          Primero -> caminar xs primero
+                                          Segundo -> caminar xs segundo
+
+caminar (x:xs) (primero :|: segundo)  = case x of
+                                          Primero -> caminar xs primero
+                                          Segundo -> caminar xs segundo
+
+
+sustituir :: Diagrama -> [Paso] -> Diagrama -> Diagrama
+sustituir d   []   _          = d
+sustituir _    _    (Hoja r)  = Hoja r
+sustituir d  (x:xs) (primero :-: segundo) = case x of
+                                            Primero -> (sustituir d xs primero) :-: segundo
+                                            Segundo -> primero :-: (sustituir d xs segundo)
+sustituir d (x:xs) (primero :|: segundo) = case x of
+                                            Primero -> (sustituir d xs primero) :|: segundo
+                                            Segundo -> primero :|: (sustituir d xs segundo)
---
+++ Proyecto1/p1-11-10428_11-10743/src/Main.hs
@@ -0,0 +1,96 @@
+module Main (main) where
+
+import Graphics.Mosaico.Diagrama (Diagrama((:-:), (:|:), Hoja), Paso(Primero, Segundo))
+import Graphics.Mosaico.Imagen   (Imagen(Imagen, altura, anchura, datos), leerImagen)
+import Graphics.Mosaico.Ventana  (Ventana, cerrar, crearVentana, leerTecla, mostrar)
+
+import System.Environment (getArgs)
+
+import Diagramas (Orientación(Horizontal, Vertical), caminar, dividir, rectánguloImagen, sustituir)
+
+
+ciclo :: Ventana -> Diagrama -> [Paso] -> IO ()
+ciclo ventana 
+    diagrama pasos 
+    = do
+        mostrar ventana pasos diagrama
+        tecla <- leerTecla ventana
+        case tecla of
+            Nothing -> putStrLn "Ventana cerrada"
+            
+            Just "q" -> cerrar ventana
+
+            Just "BackSpace" -> case pasos of 
+                [] -> continuar
+                _ -> ciclo ventana diagrama (init pasos)
+
+            Just "Up" -> case actual of
+                Nothing -> do
+                    putStrLn "Los pasos especifican un diagrama que no existe."
+                    cerrar ventana
+                Just (Hoja rectangulo) -> case divididoH rectangulo of
+                    Nothing -> continuar
+                    Just d -> ciclo ventana (sustituir d pasos diagrama) (pasos ++ [Primero])
+                Just (_ :-: _) -> ciclo ventana diagrama (pasos ++ [Primero])
+                Just (_ :|: _) -> continuar
+
+            Just "Down" -> case actual of
+                Nothing -> do
+                    putStrLn "Los pasos especifican un diagrama que no existe."
+                    cerrar ventana
+                Just (Hoja rectangulo) -> case divididoH rectangulo of
+                    Nothing -> continuar
+                    Just d -> ciclo ventana (sustituir d pasos diagrama) (pasos ++ [Segundo])
+                Just (_ :-: _) -> ciclo ventana diagrama (pasos ++ [Segundo])
+                Just (_ :|: _) -> continuar
+
+            Just "Left" -> case actual of
+                Nothing -> do
+                    putStrLn "Los pasos especifican un diagrama que no existe."
+                    cerrar ventana
+                Just (Hoja rectangulo) -> case divididoV rectangulo of
+                    Nothing -> continuar
+                    Just d -> ciclo ventana (sustituir d pasos diagrama) (pasos ++ [Primero])
+                Just (_ :|: _) -> ciclo ventana diagrama (pasos ++ [Primero])
+                Just (_ :-: _) -> continuar
+            
+            Just "Right" -> case actual of
+                Nothing -> do
+                    putStrLn "Los pasos especifican un diagrama que no existe."
+                    cerrar ventana
+                Just (Hoja rectangulo) -> case divididoV rectangulo of
+                    Nothing -> continuar
+                    Just d -> ciclo ventana (sustituir d pasos diagrama) (pasos ++ [Segundo])
+                Just (_ :|: _) -> ciclo ventana diagrama (pasos ++ [Segundo])
+                Just (_ :-: _) -> continuar
+
+            Just _ -> continuar
+    where 
+        divididoH = dividir Horizontal
+        divididoV = dividir Vertical
+        actual = caminar pasos diagrama
+        continuar = ciclo ventana diagrama pasos
+
+
+
+
+main :: IO ()
+main = do 
+        argumentos <- getArgs
+        if (length argumentos) /= 1 then
+            putStrLn "Uso: mosaico imagen"
+        else 
+            do 
+            leido <- leerImagen (head argumentos)
+            case leido of
+                Left s -> putStrLn ("Error: " ++ s)
+                Right imagen -> do 
+                    ventana <- crearVentana 600 (round ((alto imagen :: Double) /(ancho imagen :: Double)*600))
+                    let diagrama = Hoja (rectánguloImagen imagen)
+                    ciclo ventana diagrama []
+    where
+        alto = (fromInteger . altura) 
+        ancho = (fromInteger . anchura) 
+
+
+
