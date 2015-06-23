92e9e3bc4ea43b1d27df2ee73c595574c8af8047
Gustavo Gutierrez <gustavogut1993@gmail.com>
2015-06-23 18:07:30 -0430

    Comentarios agregados.

--- ProyectoProlog/tablero.pl
+++ ProyectoProlog/tablero.pl
@@ -1,3 +1,29 @@
+%% Nombre:
+%%      Proyecto de Programacion orientada a pruebas
+%%
+%% Integrantes:
+%%      Gustavo Gutierrez   11-10428
+%%      Jose Pascarella     11-10743
+%% 
+%% Descripción:
+%%      Construccion de los predicados que resulven el juego planteado
+%%      en el enunciado. 
+%%      
+%%      Dada una matriz resuelve consultas de caminos que podrian crearse.
+
+
+
+
+% tableroValido(Tablero)
+% Verifica si el tablero esta construido correctamente.
+%
+%   enteros(L)
+%   Verifica si los miembros de una lista estan en el rango [0,9].
+%   
+%   Se usaron además los predicados is_list/1, number/1, length/1
+%   como apoyo.
+
+
 enteros([]) :- !.
 enteros([X|XS]) :- 
     number(X), X < 10, X >= 0, enteros(XS). 
@@ -10,12 +36,34 @@ tableroValido([X|[Y|RS]]) :-
     tableroValido([Y|RS]), !.
 
 
+% saltoEnPosición(Tablero, N, X, Y, Sol)
+% Verdadero si en el tablero se encuentra la solución con N saltos.
+%
+% saltoEnPosición(+Tablero, +N, +X, +Y, Sol) 
+% Unifica en Sol todos los saltos posibles con los N pasos dados
+% desde la posicion (X,Y).
+%
+%   Se usaron los predicados nonvar/1, length/1
+%   
+
 saltoEnPosicion([F1|FS], N, X, Y, Sol) :-
     tableroValido([F1|FS]), 
     nonvar(X), nonvar(Y), nonvar(N), N > 0,
     length([F1|FS],LX), length(F1,LY),
     X >= 0, X =< LX, Y >= 0, Y < LY, 
     encontrar([F1|FS],N,X,Y,Sol,LX,LY).
+
+
+% salto(Tablero, N, Sol)
+% Verdadero si existe la solucion en el tablero con N pasos.
+% 
+% Ejemplo:
+%   salto(+Tablero, 2, solucion([1, 2, 3], X, Y, Pasos))
+%   Unifica en X e Y la posicion inicial y en Pasos, los saltos cardinales
+%   si el salto [1,2,3] existe en el tablero con 2 saltos 
+%
+% Se usaron los predicados nonvar/1, length/2 y between/3 este ultimo para 
+% generar numeros en un rango
 
 salto([F1|FS], N, Sol) :- 
     tableroValido([F1|FS]), nonvar(N), N >= 0,
@@ -25,6 +73,13 @@ salto([F1|FS], N, Sol) :-
     encontrar([F1|FS],N,X,Y,Sol,LX,LY).
 
 
+% encontrar/7 se uso como función auxiliar para salto y saltoEnPosición para
+% evitar el chequeo constante del tablero asi como para agregar los limites 
+% del tablero y preservarlos a traves de las llamadas.
+%
+% Se uso el predicado nth0/3 para obtener el elemento de una posición de 
+% una lista indexado en 0.  
+%
 
 encontrar(Tablero, 0, X, Y, solucion([I],X,Y,[]),LX,LY) :-
     X >= 0, X < LX, Y >= 0, Y < LY,
